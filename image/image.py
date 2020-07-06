531f4b8c6b12f87669555d9371679dcd4330fdb6
Rohit Parulkar <rohit.parulkar@gmail.com>
2020-07-05 21:33:38 -0400

    add black and flake8 pre-commit hooks

--- image/image.py
+++ image/image.py
@@ -1,18 +1,21 @@
-def get_image_from_url(url):
-    from PIL import Image
-    import requests
-    from io import BytesIO
+from typing import Optional
+from PIL import Image
+from imagekitio import ImageKit
+import requests
+from io import BytesIO
+import json
+import os
 
+
+def get_image_from_url(url: str) -> Image:
+    """returns an Image object from a given url"""
     response = requests.get(url)
     img = Image.open(BytesIO(response.content))
     return img
 
 
-def get_imagekit_urls(limit=100):
-    from imagekitio import ImageKit
-    import json
-    import os
-
+def get_imagekit_urls(limit: Optional[int] = 100) -> None:
+    """gets imagekit urls for a given set of credentials"""
     imagekit = ImageKit(
         private_key=os.environ["IMAGEKIT_PRIVATE_KEY"],
         public_key=os.environ["IMAGEKIT_PUBLIC_KEY"],
--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
-import json
 from distutils.core import setup
 
-with open('requirements.txt') as fh:
-    lines = (x.split('#')[0].strip() for x in fh)
+with open("requirements.txt") as fh:
+    lines = (x.split("#")[0].strip() for x in fh)
     requirements = [x for x in lines if not x == "-e ."]
 
 setup(
-    name='data_utils',
-    version='0.1.0',
-    description='Common utility methods I use',
-    author='Rohit Parulkar',
-    author_email='rohit.parulkar@gmail.com',
-    url='https://example.com',
-    packages=['image'],
+    name="data_utils",
+    version="0.1.0",
+    description="Common utility methods I use",
+    author="Rohit Parulkar",
+    author_email="rohit.parulkar@gmail.com",
+    url="https://example.com",
+    packages=["image"],
     install_requires=requirements,
 )
-
