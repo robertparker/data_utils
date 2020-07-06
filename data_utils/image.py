cb8bc836f33ea1b87c4d52acac8aae30720aee84
Rohit Parulkar <rohit.parulkar@gmail.com>
2020-07-06 09:13:55 -0400

    add mypy to pre-commit

--- data_utils/image.py
+++ data_utils/image.py
@@ -14,12 +14,17 @@ def get_image_from_url(url: str) -> Image:
     return img
 
 
-def get_imagekit_urls(limit: Optional[int] = 100) -> None:
+def get_imagekit_urls(
+    limit: Optional[int] = 100,
+    private_key: Optional[str] = os.environ["IMAGEKIT_PRIVATE_KEY"],
+    public_key: Optional[str] = os.environ["IMAGEKIT_PUBLIC_KEY"],
+    url_endpoint: Optional[str] = os.environ["IMAGEKIT_URL_ENDPOINT"],
+) -> None:
     """gets imagekit urls for a given set of credentials"""
+
+    # flake8: noqa E501
     imagekit = ImageKit(
-        private_key=os.environ["IMAGEKIT_PRIVATE_KEY"],
-        public_key=os.environ["IMAGEKIT_PUBLIC_KEY"],
-        url_endpoint=os.environ["IMAGEKIT_URL_ENDPOINT"],
+        private_key=private_key, public_key=public_key, url_endpoint=url_endpoint,
     )
 
     print(json.dumps(imagekit.list_files({"limit": limit})))
